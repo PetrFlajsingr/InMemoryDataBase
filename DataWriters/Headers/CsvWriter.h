bf06b63f0e631a35700a339b0f72d5a6ec45f5e1
Petr <xflajs00@stud.fit.vutbr.cz>
2018-10-13 18:37:23 +0200

    implement CsvWriter

--- DataWriters/Headers/CsvWriter.h
+++ DataWriters/Headers/CsvWriter.h
@@ -5,8 +5,26 @@
 #ifndef DATAWRITERS_HEADERS_CSVWRITER_H_
 #define DATAWRITERS_HEADERS_CSVWRITER_H_
 
-class CsvWriter {
+#include <string>
+#include <fstream>
 
+class CsvWriter {
+ private:
+  std::ofstream* outputStream;
+
+  uint64_t columnCount = 0;
+
+  const std::string DELIMITER = ";";
+
+  void writeRow(const std::vector<std::string>& data);
+ public:
+  explicit CsvWriter(std::string filePath);
+
+  virtual ~CsvWriter();
+
+  void writeHeader(const std::vector<std::string>& header);
+
+  void writeRecord(const std::vector<std::string>& record);
 };
 
 #endif  //DATAWRITERS_HEADERS_CSVWRITER_H_
--- DataWriters/Sources/CsvWriter.cpp
+++ DataWriters/Sources/CsvWriter.cpp
@@ -2,4 +2,38 @@
 // Created by Petr Flajsingr on 13/10/2018.
 //
 
-#include "../Headers/CsvWriter.h"
+#include <Exceptions.h>
+#include <vector>
+#include "CsvWriter.h"
+
+CsvWriter::CsvWriter(std::string filePath) {
+  outputStream = new std::ofstream();
+  outputStream->open(filePath, std::ofstream::out);
+}
+
+CsvWriter::~CsvWriter() {
+  outputStream->close();
+  delete outputStream;
+}
+void CsvWriter::writeHeader(const std::vector<std::string>& header) {
+  if (header.empty()) {
+    throw InvalidArgumentException("Header can not be empty.");
+  }
+
+  columnCount = header.size();
+
+  writeRow(header);
+}
+void CsvWriter::writeRecord(const std::vector<std::string>& record) {
+  if (record.size() != columnCount) {
+    throw InvalidArgumentException("Length of record doesn't match length of header.");
+  }
+
+  writeRow(record);
+}
+void CsvWriter::writeRow(const std::vector<std::string>& data) {
+  for (auto i = 0; i < columnCount - 1; ++i) {
+    *outputStream << data[i] <<  DELIMITER;
+  }
+  *outputStream << data[columnCount - 1] << "\n";
+}
