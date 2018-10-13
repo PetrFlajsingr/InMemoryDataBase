7a6fa613caa1363d22708aeb2161e74f230d2d38
Petr <xflajs00@stud.fit.vutbr.cz>
2018-10-13 18:07:17 +0200

    add CsvWriter

---
+++ DataWriters/Headers/CsvWriter.h
@@ -0,0 +1,12 @@
+//
+// Created by Petr Flajsingr on 13/10/2018.
+//
+
+#ifndef DATAWRITERS_HEADERS_CSVWRITER_H_
+#define DATAWRITERS_HEADERS_CSVWRITER_H_
+
+class CsvWriter {
+
+};
+
+#endif  //DATAWRITERS_HEADERS_CSVWRITER_H_
---
+++ DataWriters/Sources/CsvWriter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Petr Flajsingr on 13/10/2018.
+//
+
+#include "../Headers/CsvWriter.h"
