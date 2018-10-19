7b0efb4db81c855331c888d15bb93066692e10cc
Petr <xflajs00@stud.fit.vutbr.cz>
2018-10-19 16:42:50 +0200

    add FileDownloadObserver

--- FileDownloaders/Headers/BaseFileDownloader.h
+++ FileDownloaders/Headers/BaseFileDownloader.h
@@ -5,8 +5,37 @@
 #ifndef FILEDOWNLOADERS_HEADERS_BASEFILEDOWNLOADER_H_
 #define FILEDOWNLOADERS_HEADERS_BASEFILEDOWNLOADER_H_
 
-class BaseFileDownloader {
+#include <vector>
+#include <string>
+#include "Exceptions.h"
 
+class BaseFileDownloader {
+ private:
+
+ protected:
+  std::vector<std::string> availableFiles;
+
+ public:
+  virtual ~BaseFileDownloader() = default;
+
+  virtual std::vector<std::string> getAvailableFiles() {
+    return availableFiles;
+  }
+
+  virtual void downloadFile(const std::string &fileName) {
+    auto result = std::find(availableFiles.begin(),
+        availableFiles.end(),
+        fileName);
+
+    if (result == availableFiles.end()) {
+      throw InvalidArgumentException(
+          ("The file " + fileName + " is not available").c_str());
+    }
+
+    downloadFile(result - availableFiles.begin());
+  }
+
+  virtual void downloadFile(size_t fileIndex) = 0;
 };
 
 #endif  //FILEDOWNLOADERS_HEADERS_BASEFILEDOWNLOADER_H_
---
+++ FileDownloaders/Headers/FileDownloadObserver.h
@@ -0,0 +1,12 @@
+//
+// Created by Petr Flajsingr on 19/10/2018.
+//
+
+#ifndef CSV_READER_FILEDOWNLOADOBSERVER_H
+#define CSV_READER_FILEDOWNLOADOBSERVER_H
+
+class FileDownloadObserver {
+
+};
+
+#endif //CSV_READER_FILEDOWNLOADOBSERVER_H
