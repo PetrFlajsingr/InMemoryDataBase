387c7429084c647ac0ed3d242910fdd9ad3bd81d
Petr <xflajs00@stud.fit.vutbr.cz>
2018-10-19 16:32:08 +0200

    add BaseFileDownloader

---
+++ FileDownloaders/Headers/BaseFileDownloader.h
@@ -0,0 +1,12 @@
+//
+// Created by Petr Flajsingr on 19/10/2018.
+//
+
+#ifndef CSV_READER_BASEFILEDOWNLOADER_H
+#define CSV_READER_BASEFILEDOWNLOADER_H
+
+class BaseFileDownloader {
+
+};
+
+#endif //CSV_READER_BASEFILEDOWNLOADER_H
