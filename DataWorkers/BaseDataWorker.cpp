a8440accfa32733f8e0840c78af5fe5e63771937
Petr <xflajs00@stud.fit.vutbr.cz>
2018-11-12 16:12:59 +0100

    implement BaseDataWorker

--- DataWorkers/BaseDataWorker.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Petr Flajsingr on 12/11/2018.
-//
-
-#include "BaseDataWorker.h"
---
+++ DataWorkers/Headers/BaseDataWorker.h
@@ -0,0 +1,39 @@
+//
+// Created by Petr Flajsingr on 12/11/2018.
+//
+
+#ifndef CSV_READER_BASEDATAWORKER_H
+#define CSV_READER_BASEDATAWORKER_H
+
+#include <string>
+#include <CsvWriter.h>
+#include <BaseDataSet.h>
+#include <FilterStructures.h>
+
+namespace DataWorkers {
+class BaseDataWorker {
+ protected:
+  CsvWriter* writer = nullptr;
+
+  DataSets::BaseDataSet* dataset = nullptr;
+ public:
+  BaseDataWorker(CsvWriter *writer, DataProviders::BaseDataProvider* dataProvider) : writer(writer) {}
+
+  virtual ~BaseDataWorker() {
+    delete writer;
+    delete dataset;
+  }
+
+  virtual std::string getMultiChoiceNames() = 0;
+
+  virtual std::string getChoices(std::string choiceName) = 0;
+
+  void setDataWriter(CsvWriter& writer) {
+    this->writer = &writer;
+  }
+
+  virtual void setFilters(std::vector<DataSets::FilterOptions> filters)=0;
+};
+} // namespace DataWorkers
+
+#endif  //CSV_READER_BASEDATAWORKER_H
