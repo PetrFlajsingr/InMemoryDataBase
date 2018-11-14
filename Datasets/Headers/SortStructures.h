c535f1800c701d195f47f8c8c4490b0ecb75f43a
Petr <xflajs00@stud.fit.vutbr.cz>
2018-11-14 17:14:47 +0100

    implement SortStructures

--- Datasets/Headers/SortStructures.h
+++ Datasets/Headers/SortStructures.h
@@ -5,4 +5,27 @@
 #ifndef CSV_READER_SORTSTRUCTURES_H
 #define CSV_READER_SORTSTRUCTURES_H
 
+#include <cstdint>
+#include <Types.h>
+#include <vector>
+namespace DataSets {
+
+struct SortItem {
+  uint64_t fieldIndex;
+  SortOrder order;
+};
+
+struct SortOptions {
+  std::vector<SortItem> options;
+
+  void addOption(uint64_t index, SortOrder order) {
+    SortItem item {
+      index,
+      order};
+    options.push_back(item);
+  }
+};
+
+}  // namespace DataSets
+
 #endif //CSV_READER_SORTSTRUCTURES_H
