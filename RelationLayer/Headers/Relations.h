3fc3d768d3402f0de022e41f53f21282bba492c7
Petr <xflajs00@stud.fit.vutbr.cz>
2019-01-29 16:46:58 +0100

    add RelationContainer

---
+++ RelationLayer/Headers/Relations.h
@@ -0,0 +1,38 @@
+//
+// Created by Petr Flajsingr on 2019-01-29.
+//
+
+#ifndef CSV_READER_BASERELATION_H
+#define CSV_READER_BASERELATION_H
+
+#include <Types.h>
+
+namespace RelationLayer {
+
+struct RelationContainer {
+  DataContainer value_FirstDataSet;
+  DataContainer value_SecondDataSet;
+};
+
+class BaseRelation {
+ protected:
+
+ public:
+
+};
+
+class OneToOneRelation : public BaseRelation {
+
+};
+
+class OneToNRelation : public BaseRelation {
+
+};
+
+class NToNRelation : public BaseRelation {
+
+};
+
+}  // namespace RelationLayer
+
+#endif //CSV_READER_BASERELATION_H
