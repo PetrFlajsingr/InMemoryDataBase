c5060336efecab89c9ab518fdb66b09f6198def8
Petr <xflajs00@stud.fit.vutbr.cz>
2019-01-29 16:56:31 +0100

    implement BaseRelation

--- RelationLayer/Headers/Relations.h
+++ RelationLayer/Headers/Relations.h
@@ -11,26 +11,49 @@
 namespace RelationLayer {
 
 struct RelationContainer {
-  DataContainer value_FirstDataSet;
-  DataContainer value_SecondDataSet;
+  void *data_FirstDataSet;
+  void *data_SecondDataSet;
 };
 
 class BaseRelation {
  protected:
   std::vector<RelationContainer> relations;
- public:
 
+ public:
+  virtual void addRelation(void *dataFirstDataSet, void *dataSecondDataSet) = 0;
+
+  virtual void removeRelation(void *dataFirstDataSet, void *dataSecondDataSet) = 0;
+
+  virtual const std::vector<RelationContainer> &findRelations(void *pointerToData) = 0;
 };
 
 class OneToOneRelation : public BaseRelation {
+ public:
+  void addRelation(void *dataFirstDataSet, void *dataSecondDataSet) override;
+
+  void removeRelation(void *dataFirstDataSet, void *dataSecondDataSet) override;
+
+  const std::vector<RelationContainer> &findRelations(void *pointerToData) override;
 
 };
 
 class OneToNRelation : public BaseRelation {
+ public:
+  void addRelation(void *dataFirstDataSet, void *dataSecondDataSet) override;
+
+  void removeRelation(void *dataFirstDataSet, void *dataSecondDataSet) override;
+
+  const std::vector<RelationContainer> &findRelations(void *pointerToData) override;
 
 };
 
 class NToNRelation : public BaseRelation {
+ public:
+  void addRelation(void *dataFirstDataSet, void *dataSecondDataSet) override;
+
+  void removeRelation(void *dataFirstDataSet, void *dataSecondDataSet) override;
+
+  const std::vector<RelationContainer> &findRelations(void *pointerToData) override;
 
 };
 
--- RelationLayer/Source/Relations.cpp
+++ RelationLayer/Source/Relations.cpp
@@ -3,3 +3,40 @@
 //
 
 #include <Relations.h>
+#include <Exceptions.h>
+
+void RelationLayer::OneToOneRelation::addRelation(void *dataFirstDataSet, void *dataSecondDataSet) {
+  throw NotImplementedException();
+}
+
+void RelationLayer::OneToOneRelation::removeRelation(void *dataFirstDataSet, void *dataSecondDataSet) {
+  throw NotImplementedException();
+}
+
+const std::vector<RelationLayer::RelationContainer> &RelationLayer::OneToOneRelation::findRelations(void *pointerToData) {
+  throw NotImplementedException();
+}
+
+void RelationLayer::OneToNRelation::addRelation(void *dataFirstDataSet, void *dataSecondDataSet) {
+  throw NotImplementedException();
+}
+
+void RelationLayer::OneToNRelation::removeRelation(void *dataFirstDataSet, void *dataSecondDataSet) {
+  throw NotImplementedException();
+}
+
+const std::vector<RelationLayer::RelationContainer> &RelationLayer::OneToNRelation::findRelations(void *pointerToData) {
+  throw NotImplementedException();
+}
+
+void RelationLayer::NToNRelation::addRelation(void *dataFirstDataSet, void *dataSecondDataSet) {
+  throw NotImplementedException();
+}
+
+void RelationLayer::NToNRelation::removeRelation(void *dataFirstDataSet, void *dataSecondDataSet) {
+  throw NotImplementedException();
+}
+
+const std::vector<RelationLayer::RelationContainer> &RelationLayer::NToNRelation::findRelations(void *pointerToData) {
+  throw NotImplementedException();
+}
