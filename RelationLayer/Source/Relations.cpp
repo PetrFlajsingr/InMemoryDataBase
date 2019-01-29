8ef8ccdd9c6fc97c3389816d26d6cb87a5f8c95d
Petr <xflajs00@stud.fit.vutbr.cz>
2019-01-29 17:00:00 +0100

    refactoring

--- RelationLayer/Source/Relations.cpp
+++ RelationLayer/Source/Relations.cpp
@@ -5,41 +5,52 @@
 #include <Relations.h>
 #include <Exceptions.h>
 
-RelationLayer::RelationContainer::RelationContainer(void *data_FirstDataSet, void *data_SecondDataSet)
-    : data_FirstDataSet(data_FirstDataSet), data_SecondDataSet(data_SecondDataSet) {}
+RelationLayer::RelationContainer::RelationContainer(void *data_FirstDataSet,
+                                                    void *data_SecondDataSet)
+    : data_FirstDataSet(data_FirstDataSet),
+      data_SecondDataSet(data_SecondDataSet) {}
 
-void RelationLayer::OneToOneRelation::addRelation(void *dataFirstDataSet, void *dataSecondDataSet) {
+void RelationLayer::OneToOneRelation::addRelation(void *dataFirstDataSet,
+                                                  void *dataSecondDataSet) {
   relations.emplace_back(dataFirstDataSet, dataSecondDataSet);
 }
 
-void RelationLayer::OneToOneRelation::removeRelation(void *dataFirstDataSet, void *dataSecondDataSet) {
+void RelationLayer::OneToOneRelation::removeRelation(void *dataFirstDataSet,
+                                                     void *dataSecondDataSet) {
   throw NotImplementedException();
 }
 
-const std::vector<RelationLayer::RelationContainer> &RelationLayer::OneToOneRelation::findRelations(void *pointerToData) {
+const std::vector<RelationLayer::RelationContainer> &RelationLayer::OneToOneRelation::findRelations(
+    void *pointerToData) {
   throw NotImplementedException();
 }
 
-void RelationLayer::OneToNRelation::addRelation(void *dataFirstDataSet, void *dataSecondDataSet) {
+void RelationLayer::OneToNRelation::addRelation(void *dataFirstDataSet,
+                                                void *dataSecondDataSet) {
   relations.emplace_back(dataFirstDataSet, dataSecondDataSet);
 }
 
-void RelationLayer::OneToNRelation::removeRelation(void *dataFirstDataSet, void *dataSecondDataSet) {
+void RelationLayer::OneToNRelation::removeRelation(void *dataFirstDataSet,
+                                                   void *dataSecondDataSet) {
   throw NotImplementedException();
 }
 
-const std::vector<RelationLayer::RelationContainer> &RelationLayer::OneToNRelation::findRelations(void *pointerToData) {
+const std::vector<RelationLayer::RelationContainer> &RelationLayer::OneToNRelation::findRelations(
+    void *pointerToData) {
   throw NotImplementedException();
 }
 
-void RelationLayer::NToNRelation::addRelation(void *dataFirstDataSet, void *dataSecondDataSet) {
+void RelationLayer::NToNRelation::addRelation(void *dataFirstDataSet,
+                                              void *dataSecondDataSet) {
   relations.emplace_back(dataFirstDataSet, dataSecondDataSet);
 }
 
-void RelationLayer::NToNRelation::removeRelation(void *dataFirstDataSet, void *dataSecondDataSet) {
+void RelationLayer::NToNRelation::removeRelation(void *dataFirstDataSet,
+                                                 void *dataSecondDataSet) {
   throw NotImplementedException();
 }
 
-const std::vector<RelationLayer::RelationContainer> &RelationLayer::NToNRelation::findRelations(void *pointerToData) {
+const std::vector<RelationLayer::RelationContainer> &RelationLayer::NToNRelation::findRelations(
+    void *pointerToData) {
   throw NotImplementedException();
 }
