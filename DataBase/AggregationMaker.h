022a0dfd193eda41df2dd9b75b6bf7ef9ddf9ce3
Petr <xflajs00@stud.fit.vutbr.cz>
2019-02-21 08:35:55 +0100

    add DataBase::AggregationMaker

---
+++ DataBase/AggregationMaker.h
@@ -0,0 +1,14 @@
+//
+// Created by Petr Flajsingr on 2019-02-21.
+//
+
+#ifndef PROJECT_AGGREGATONMAKER_H
+#define PROJECT_AGGREGATONMAKER_H
+
+namespace DataBase {
+class AggregationMaker {
+
+};
+}
+
+#endif //PROJECT_AGGREGATONMAKER_H
