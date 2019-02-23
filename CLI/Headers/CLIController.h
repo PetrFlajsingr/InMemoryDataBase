ad2e39fc74f673e115e7449e82b8e65418286dd7
Petr <xflajs00@stud.fit.vutbr.cz>
2019-02-23 13:39:53 +0100

    add CLIController

---
+++ CLI/Headers/CLIController.h
@@ -0,0 +1,12 @@
+//
+// Created by Petr Flajsingr on 2019-02-23.
+//
+
+#ifndef PROJECT_CLICONTROLLER_H
+#define PROJECT_CLICONTROLLER_H
+
+class CLIController {
+
+};
+
+#endif //PROJECT_CLICONTROLLER_H
---
+++ CLI/Source/CLIController.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Petr Flajsingr on 2019-02-23.
+//
+
+#include "CLIController.h"
