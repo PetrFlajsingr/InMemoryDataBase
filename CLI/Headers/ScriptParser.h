dd5f793e3191daa58d1f8caf77e49972f6c4eaaf
Petr <xflajs00@stud.fit.vutbr.cz>
2019-02-23 13:38:18 +0100

    add ScriptParser

---
+++ CLI/Headers/ScriptParser.h
@@ -0,0 +1,12 @@
+//
+// Created by Petr Flajsingr on 2019-02-23.
+//
+
+#ifndef PROJECT_SCRIPTPARSER_H
+#define PROJECT_SCRIPTPARSER_H
+
+class ScriptParser {
+
+};
+
+#endif //PROJECT_SCRIPTPARSER_H
---
+++ CLI/Source/ScriptParser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Petr Flajsingr on 2019-02-23.
+//
+
+#include "ScriptParser.h"
