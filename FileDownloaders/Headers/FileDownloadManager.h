7bf3881ca7a94a7c7cf01171584e061501bc64d2
Petr <xflajs00@stud.fit.vutbr.cz>
2019-02-24 14:11:16 +0100

    add FileDownloadManager

---
+++ FileDownloaders/Headers/FileDownloadManager.h
@@ -0,0 +1,12 @@
+//
+// Created by Petr Flajsingr on 2019-02-24.
+//
+
+#ifndef PROJECT_FILEDOWNLOADMANAGER_H
+#define PROJECT_FILEDOWNLOADMANAGER_H
+
+class FileDownloadManager {
+
+};
+
+#endif //PROJECT_FILEDOWNLOADMANAGER_H
