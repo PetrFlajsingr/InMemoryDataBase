91ea83a2bb26c4972c0c1fbce36f16b1e3bb4ad4
Petr <xflajs00@stud.fit.vutbr.cz>
2019-03-20 17:00:27 +0100

    Observable refactoring

--- Templates/Headers/Observable.h
+++ Templates/Headers/Observable.h
@@ -22,6 +22,10 @@ class ObservableBase;
  */
 class Observer {
  public:
+  /**
+   * Called when an observed value is changed.
+   * @param obs Observable whose value has changed
+   */
   virtual void update(ObservableBase const *obs) = 0;
 };
 /**
@@ -30,30 +34,59 @@ class Observer {
  */
 class ObservableBase {
  public:
+  /**
+   * Add observer whose update method will be called when this's value changes.
+   * @param observer observer to notify on value change
+   */
   void addObserver(Observer *observer) {
     observers.emplace_back(observer);
   }
+  /**
+   * Delete observer to prevent further update notifications.
+   * @param observer Observer to delete from notify list
+   */
   void deleteObserver(Observer *observer) {
     if (auto it = std::find(observers.begin(), observers.end(), observer); it
         != observers.end()) {
       observers.erase(it);
     }
   }
+  /**
+   * Clear all observers, preventing further notifications of change to any of already registered.
+   */
   void deleteObservers() {
     observers.clear();
   }
+  /**
+   *
+   * @return count of observers currently watching this's value
+   */
   size_t countObservers() {
     return observers.size();
   }
+  /**
+   *
+   * @return true if value has been changed, false otherwise
+   */
   bool hasChanged() {
     return changed;
   }
+  /**
+   * Disable observer notification until enable() is called.
+   */
   void disable() {
     enabled = false;
   }
+  /**
+   * Enable observer notification. On by default.
+   */
   void enable() {
     enabled = true;
   }
+  /**
+   * Notify observers of a value change in this.
+   * The changed and enabled flags have to be set for the notifications to go through.
+   */
   void notifyObservers() {
     if (changed && enabled) {
       std::for_each(observers.begin(), observers.end(), [this](auto observer) {
@@ -79,6 +112,11 @@ class Observable
  public:
   Observable() = default;
   explicit Observable(T value) : value(value) {}
+  /**
+   * Assign value and notify observers.
+   * Doesn't notify observers if disable() has been called.
+   * @param f new value
+   */
   typename std::conditional<std::is_compound<T>::value, T &, T>::type operator=(
       const T &f) {
     value = f;
@@ -86,6 +124,9 @@ class Observable
     notifyObservers();
     return value;
   }
+  /**
+   * Implicit cast to stored value.
+   */
   operator typename std::conditional<std::is_compound<T>::value,
                                      const T &,
                                      T>::type() const { return value; }
