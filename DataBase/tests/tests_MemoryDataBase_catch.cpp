382609c74f01e61709b9d113ca12e84b4cc853b5
Igor Frank <ifrank@seznam.cz>
2019-10-04 21:29:49 +0200

    Database testcase reads from string array

--- DataBase/tests/tests_MemoryDataBase_catch.cpp
+++ DataBase/tests/tests_MemoryDataBase_catch.cpp
@@ -3,40 +3,45 @@
 //
 
 #include <CsvReader.h>
+#include <ArrayDataProvider.h>
 #include <catch2/catch.hpp>
 #include <MemoryDataBase.h>
 #include <MemoryDataSet.h>
+#include <stdexcept>
+#include "testTableString.h"
+#include <DoubleField.h>
 
 #define TABLE1_COLUMN_COUNT 3
 #define TABLE1_ROW_COUNT 3
 
-SCENARIO("Operations with DB", "[MemoryDataBase]"){
-    GIVEN("Basic tables"){
-        const std::string dbData[TABLE1_ROW_COUNT][TABLE1_COLUMN_COUNT]{
-                {"0","\"Johny\"","\"Boy\""},
-                {"1","\"Peter\"","\"Griffin\""},
-                {"2","\"Bruce\"","\"Don\""}
-        };
+SCENARIO("Operations with DB", "[MemoryDataBase]") {
+    GIVEN("Basic tables") {
 
         const std::string query = "SELECT table1.* FROM table1;";
 
-        DataProviders::CsvReader csvReader("../DataBase/tests/files/basic_table.csv",
-                                           ",");
+        DataProviders::ArrayDataProvider dataProvider(TestTables::testTable1);
         DataBase::MemoryDataBase db("testDB");
 
-        WHEN("Adding table"){
+        WHEN("Adding table") {
             auto dsTable1 = std::make_shared<DataSets::MemoryDataSet>("table1");
-            dsTable1->open(csvReader, {ValueType::Integer,
-                                       ValueType::String,
-                                       ValueType::String});
+            dsTable1->open(dataProvider, {ValueType::Integer,
+                                          ValueType::Double,
+                                          ValueType::Currency,
+                                          ValueType::String,
+                                          ValueType::String,
+                                          ValueType::String,
+                                          ValueType::String,
+                                          ValueType::String,
+                                          ValueType::String,
+                                          ValueType::String});
             db.addTable(dsTable1);
 
-            THEN("Correct values are in table"){
+            THEN("Correct values are in table") {
                 auto result = db.execSimpleQuery(query, false, "table1View");
                 auto rowCount = 0;
-                while(result->dataSet->next()){
-                    for(auto i = 0; i < TABLE1_COLUMN_COUNT; ++i) {
-                       REQUIRE(result->dataSet->fieldByIndex(i)->getAsString() == dbData[rowCount][i]);
+                while (result->dataSet->next()) {
+                    for (auto i = 0; i < static_cast<int>(TestTables::testTable1[0].size()); ++i) {
+                        CHECK(result->dataSet->fieldByIndex(i)->getAsString() == TestTables::testTable1[rowCount][i]);
                     }
                     ++rowCount;
                 }
