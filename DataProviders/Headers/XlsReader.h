a33690216072208c8677c0abc777dc4fe41ab7d5
PetrFlajsingr <flajsingr@gmail.com>
2019-10-08 18:16:16 +0200

    create XlsReader

---
+++ DataProviders/Headers/XlsReader.h
@@ -0,0 +1,10 @@
+//
+// Created by petr on 10/8/19.
+//
+
+#ifndef CSV_READER_XLSREADER_H
+#define CSV_READER_XLSREADER_H
+
+class XlsReader {};
+
+#endif // CSV_READER_XLSREADER_H
---
+++ DataProviders/Source/XlsReader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by petr on 10/8/19.
+//
+
+#include "XlsReader.h"
