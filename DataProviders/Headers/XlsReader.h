3f328e68ef810a26eed54e6eb6ed15d633c013b6
PetrFlajsingr <flajsingr@gmail.com>
2019-10-08 18:18:48 +0200

    add nodiscard

--- DataProviders/Headers/XlsReader.h
+++ DataProviders/Headers/XlsReader.h
@@ -10,14 +10,21 @@
 namespace DataProviders {
 class XlsReader : public BaseDataProvider {
 public:
-  const std::vector<std::string> &getRow() const override;
-  std::string getColumnName(unsigned int columnIndex) const override;
-  uint64_t getColumnCount() const override;
-  const std::vector<std::string> &getHeader() const override;
-  int getCurrentRecordNumber() const override;
+  [[nodiscard]] const std::vector<std::string> &getRow() const override;
+
+  [[nodiscard]] std::string getColumnName(unsigned int columnIndex) const override;
+
+  [[nodiscard]] uint64_t getColumnCount() const override;
+
+  [[nodiscard]] const std::vector<std::string> &getHeader() const override;
+
+  [[nodiscard]] int getCurrentRecordNumber() const override;
+
   bool next() override;
+
   void first() override;
-  bool eof() const override;
+
+  [[nodiscard]] bool eof() const override;
 };
 }
 
